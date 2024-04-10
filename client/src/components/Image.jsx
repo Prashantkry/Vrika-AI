4417db005e6f527b68b713b4e747f84bb943239d
Prashant Kumar <71703153+Prashantkry@users.noreply.github.com>
2024-04-10 16:20:11 +0530

    image generations ui created

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -3,6 +3,8 @@ import w2 from "../assets/w2.png";
 import w3 from "../assets/w3.png";
 import w4 from "../assets/w4.png";
 import w5 from "../assets/w5.png";
+import down from "../assets/down.svg";
+import right from "../assets/right.svg";
 
 export default w2;
-export { w1, w3, w4,w5 };
+export { w1, w3, w4,w5,down,right };
