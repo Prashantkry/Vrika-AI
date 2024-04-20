11fd2dabd7b96860826b306add774d74135785fc
Prashant Kumar <71703153+Prashantkry@users.noreply.github.com>
2024-04-21 00:39:56 +0530

    use case and new sign in & up page generation divided e in 2 category text to image and image or 3d model to image

--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Features = () => {
   return (
     <>
+      <div>
         <div>
-            
+          <p>
+            Vrika AI is a set of tools that help designers and architects to
+            create better designs and make better decisions.
+          </p>
         </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Features
+export default Features;
