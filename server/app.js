8ee43578246a3b7ad43c5dd218008d590a7a9480
Prashant Kumar <71703153+Prashantkry@users.noreply.github.com>
2024-05-19 19:21:55 +0530

    .

--- server/app.js
+++ server/app.js
@@ -27,13 +27,3 @@ app.get("/", (req, res) => {
 });
 
 module.exports = app;
-
-
-/*
-
-curl -X POST https://run.cerebrium.ai/v3/p-132491f2/vrikaaicerebriumdata/predict \
-     -H 'Content-Type: application/json'\
-     -H 'Authorization: eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0SWQiOiJwLTEzMjQ5MWYyIiwiaWF0IjoxNzEyMzQyMzkwLCJleHAiOjIwMjc5MTgzOTB9.lNmoyP_iwUkeGBPRd9zJ3xMJ9Ec9nNzWgRDMy3jZKjnlVsgvbDYihLtKB0673hEv7D9NiYZP5HZVUa9KQwWtyjvv59zmHMMk8Ph_IilicQfzR7P5Ywtovdz389GCEMW5PERnqsbQspPVQx3JHCUVmVkRxDaSTBGDIyEqy8cFiBHeNvDHCYzuEsCrN3rFHOB18p0CYPUklYbo3SJXLhYToKjxYOR8jz_z5LcuqdWHhX3fAW40ytbbENK7w6h55V4GBY2NkiTgqo23XKrF8ZVkj7N04-uCBXiI5PlOdeiXjGJi2WbXqWO5yl85Ejd8vy_XI1DY1MLzHcaMTWwkakcdsA'\
-     --data '{"prompt": "Hello World!"}'
-
-*/
