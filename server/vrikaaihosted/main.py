aaf3cebaba986884ca82a128a7ee1845ae51b3a5
Prashant Kumar <71703153+Prashantkry@users.noreply.github.com>
2024-05-20 15:52:56 +0530

    cerebrium data change

---
+++ server/vrikaaihosted/main.py
@@ -0,0 +1,38 @@
+import base64
+import io
+import torch
+
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+from typing import Optional
+from pydantic import BaseModel
+
+# Specify the model to be loaded
+model_id = 'stabilityai/stable-diffusion-2-1'
+
+# Load the pipeline from the pre-trained model
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+
+# Optimize for speed and efficiency by using a multistep scheduler
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+
+# Move model and computations to GPU for faster processing
+pipe = pipe.to('cuda')
+
+class Item(BaseModel):
+	prompt: str
+	num_inference_steps: Optional[int] = 50
+ 
+def predict(item, run_id, logger):
+    item = Item(**item)
+
+    image = pipe(item.prompt).images[0]
+    buffered = io.BytesIO()
+    image.save(buffered, format='PNG')
+    finished_image = base64.b64encode(buffered.getvalue()).decode('utf-8')
+    return finished_image
+
+def run(param_1: str, param_2: str, run_id):  # run_id is optional, injected by Cerebrium at runtime
+    my_results = {"1": param_1, "2": param_2}
+    my_status_code = 200 # if you want to return a specific status code
+
+    return {"my_result": my_results, "status_code": my_status_code} # return your results
